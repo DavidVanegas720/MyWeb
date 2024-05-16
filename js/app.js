db53d5ec30e9ff0961f16a7b9fd1d33014038915
DavidVanegas720 <david_1069@hotmail.com>
2024-05-16 16:30:51 -0500

    agrego codigo js para una funcion del z-index

--- js/app.js
+++ js/app.js
@@ -3,3 +3,21 @@
 document.getElementById("contactButton").addEventListener("click", () =>{
     window.location.href = "mailto:jdavidvanegas25@gmail.com"
 });
+
+// -------css mostrar text z-index----------------
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const projectLinks = document.querySelectorAll('.content-proyects-links');
+  
+    projectLinks.forEach(link => {
+      link.addEventListener('mouseover', () => {
+        const overlayText = link.querySelector('.overlay-text');
+        overlayText.classList.add('show');
+      });
+  
+      link.addEventListener('mouseout', () => {
+        const overlayText = link.querySelector('.overlay-text');    
+        overlayText.classList.remove('show');
+      });
+    });
+  });
